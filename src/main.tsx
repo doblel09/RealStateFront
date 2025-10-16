840fde861a3395add90dcadd521a666da0faf32f
Luis Florentino <luisflorentino1999@gmail.com>
2025-10-15 22:06:33 -0400

    refactor: private routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
 import HomePage from '@/pages/HomePage.tsx'
 import Layout from '@/pages/Layout'
 import Details from '@/pages/Details'
@@ -14,10 +13,11 @@ import Rent from '@/pages/Rent.tsx'
 import Agents from '@/pages/Agents.tsx'
 import Agent from '@/pages/Agent.tsx'
 import Listings from '@/pages/Listings'
-import { AuthProvider } from './hooks/useAuth'
-import PrivateRoute from './pages/PrivateRoute'
-import PublicRoute from './pages/PublicRoute'
-import ManageUser from './pages/ManageUser'
+import { AuthProvider } from '@/hooks/useAuth'
+import PrivateRoute from '@/pages/PrivateRoute'
+import PublicRoute from '@/pages/PublicRoute'
+import ManageUser from '@/pages/ManageUser'
+import Profile from '@/pages/Profile'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -28,17 +28,20 @@ createRoot(document.getElementById('root')!).render(
           <Route path="/" element={<Layout />}>
             <Route index element={<HomePage />} />
             <Route path="details" element={<Details />} />
-            <Route path="login" element={<PublicRoute><Login /></PublicRoute>} />
-            <Route path="sign-up" element={<PublicRoute><SignUp /></PublicRoute>} />
-            <Route path="manage-property" element={<PrivateRoute><ManageProperty /></PrivateRoute>} />
             <Route path="buy" element={<Buy />} />
             <Route path="rent" element={<Rent />} />
             <Route path="agents" element={<Agents />} />
             <Route path="agent/:id" element={<Agent />} />
-            <Route path="listings" element={<PrivateRoute><Listings /></PrivateRoute>} />
-            <Route path="create-listing" element={<PrivateRoute><ManageProperty /></PrivateRoute>} />
-            <Route path="manage-listing/:id" element={<PrivateRoute><ManageProperty /></PrivateRoute>} />
-            <Route path="manage-account" element={<PrivateRoute><ManageUser /></PrivateRoute>} />
+            <Route element={<PrivateRoute />}>
+              <Route path="manage-property" element={<ManageProperty />} />
+              <Route path="listings" element={<Listings />} />
+              <Route path="create-listing" element={<ManageProperty />} />
+              <Route path="manage-listing/:id" element={<ManageProperty />} />
+              <Route path="manage-account" element={<ManageUser />} />
+              <Route path="profile" element={<Profile />} />
+            </Route>
+           <Route path="login" element={<PublicRoute><Login /></PublicRoute>} />
+          <Route path="sign-up" element={<PublicRoute><SignUp /></PublicRoute>} />
           </Route>
         </Routes>
       </AuthProvider>
